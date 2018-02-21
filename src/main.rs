e5fd0ca9a4d056f5d9212bd4314d7f8b521c70d2
jonathan gerber <jlgerber@gmail.com>
2018-02-20 19:19:08 -0800

    updated executable to pay attention to LEVELSPEC_UPPER env var

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,13 @@ fn main() {
     if args.len() < 2 {
         println!("usage: levelspec <level>");
     } else {
-        if let Ok(ls) = LevelSpec::new(&args[1]){
+        // fetch LEVELSPEC_UPPER
+        if let Ok(mut ls) = LevelSpec::new(&args[1]) {
+            if let Ok(val) = env::var("LEVELSPEC_UPPER") {
+                if val == "1" || val == "true" || val == "yes" {
+                    ls.upper();
+                }
+        }
             println!("{:?}", ls);
         } else {
             println!("Error parsing {}", &args[1]);
