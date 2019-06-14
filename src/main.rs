0668c99e66b27a6c3dcd5d9f800feba5c6ea8a83
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-14 07:46:38 -0700

    ups version to 3 and modernizes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-extern crate levelspec;
-use levelspec::*;
+use levelspec::LevelSpec;
 use std::env;
 
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("usage: levelspec <level>");
-    } else {
-        // fetch LEVELSPEC_UPPER
-        if let Ok(mut ls) = LevelSpec::new(&args[1]) {
-            if let Ok(val) = env::var("LEVELSPEC_UPPER") {
-                if val == "1" || val == "true" || val == "yes" {
-                    ls.upper();
-                }
-        }
-            println!("{:?}", ls);
-        } else {
-            println!("Error parsing {}", &args[1]);
+        std::process::exit(1);
+    } 
+    let mut ls = LevelSpec::new(&args[1])?;
+    if let Ok(val) = env::var("LEVELSPEC_UPPER") {
+        if val == "1" || val == "true" || val == "yes" {
+            ls.upper();
         }
     }
+
+    println!("{:?}", ls);
+     
+    Ok(())
 }
